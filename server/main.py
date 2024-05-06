f29eb968455ea0e49b13bda76d70ad5e139b85f8
Daniil Mikhaylov <www.daniiland@gmail.com>
2024-05-07 02:24:50 +0800

    0_o

--- server/main.py
+++ server/main.py
@@ -3,11 +3,13 @@ from sqlalchemy.orm import Session
 
 import models
 from database import SessionLocal, engine
+from sqlalchemy import MetaData
+
+metadata = MetaData()
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 # Dependency
 def get_db():
@@ -16,5 +18,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
